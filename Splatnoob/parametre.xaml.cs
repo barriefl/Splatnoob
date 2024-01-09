9963fa02137e1c9d4f6976b658e7a4e0bc5e5b27
leo levet <leo.levet@etu.univ-smb.fr>
2024-01-09 17:35:48 +0100

    ajout de l'ajustement du temps paramètre

--- Splatnoob/parametre.xaml.cs
+++ Splatnoob/parametre.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Splatnoob
 {
@@ -19,9 +20,22 @@ namespace Splatnoob
     /// </summary>
     public partial class parametre : Window
     {
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private int TEMPSPARDEPLACAGE = 15;
+        private int NBRSECMIN = 60;
         public parametre()
         {
             InitializeComponent();
+            dispatcherTimer.Tick += actualisation;
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(16);
+            dispatcherTimer.Start();
+        }
+        private void actualisation(object sender, EventArgs e)
+        {
+            double tps_total = TEMPSPARDEPLACAGE * Math.Round(choisirtps.Value);
+            double tps_min = Math.Truncate(tps_total / NBRSECMIN);
+            double tps_sec = (tps_total - (tps_min * NBRSECMIN));
+            affichtps.Content = (tps_min + ":" + tps_sec);
         }
     }
 }
